99fbbfc23e4559a34af38f21a7611ead9f4f7460
theepag <theepag@live.com>
2020-05-28 13:28:23 +0530

    Updates

---
+++ precache-manifest.66cb9cf8068e2858157bf7d6a4e740ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb71331361d3dac3e9369390e206b7c",
+    "url": "/CoronaLK/index.html"
+  },
+  {
+    "revision": "09032e12fdeb8ec83222",
+    "url": "/CoronaLK/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "09032e12fdeb8ec83222",
+    "url": "/CoronaLK/static/js/2.536f040c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoronaLK/static/js/2.536f040c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464d1af20925c6c1a220",
+    "url": "/CoronaLK/static/js/main.cb2bcf16.chunk.js"
+  },
+  {
+    "revision": "23b609bfc08dff3093be",
+    "url": "/CoronaLK/static/js/runtime-main.54003ece.js"
+  }
+]);
