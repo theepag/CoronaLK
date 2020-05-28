97a7fec0ba95c034199239ef86851f2ed750f61b
theepag <theepag@live.com>
2020-05-28 13:11:46 +0530

    simple interface added

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
 class Viewer extends React.Component {
   constructor(props) {
     super(props);
@@ -12,13 +10,13 @@ class Viewer extends React.Component {
   }
 
   componentDidMount() {
-    fetch("https://api.example.com/items")
+    fetch("https://www.hpb.health.gov.lk/api/get-current-statistical")
       .then((res) => res.json())
       .then(
         (result) => {
           this.setState({
             isLoaded: true,
-            items: result.items,
+            items: result.data,
           });
         },
         // Note: it's important to handle errors here
@@ -41,13 +39,33 @@ class Viewer extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <ul>
-          {items.map((item) => (
-            <li key={item.name}>
-              {item.name} {item.price}
-            </li>
-          ))}
-        </ul>
+        <div>
+          <div style={{ textAlign: "center", marginTop: 100 }}>
+            <h1> Covid Sri Lanka </h1> <br />
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/765px-SARS-CoV-2_without_background.png"
+              height="150"
+            ></img>
+          </div>
+          <br />
+          <div class="container">
+            <div class="row" style={{ fontSize: 27 }}>
+              <div class="col-sm">
+                Total confirmed cases <br />
+                {items.local_total_cases}
+              </div>
+              <div class="col-sm">
+                Deaths <br />
+                <span style={{ color: "red" }}>{items.local_deaths}</span>
+              </div>
+              <div class="col-sm">
+                Recovered & Discharged
+                <br />
+                <span style={{ color: "green" }}>{items.local_recovered} </span>
+              </div>
+            </div>
+          </div>
+        </div>
       );
     }
   }
